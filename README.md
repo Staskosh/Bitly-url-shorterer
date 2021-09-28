141fb4af682d78a96ae35902d583148efd2cc4be
staskosh <staskoshenkov@rambler.ru>
2021-09-28 16:17:50 +0300

    Release 1

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Bitly url shorterer
- 
+#Bitly url shorterer
+
+Input some URL to get a Bitly short link and forget about long URLs. 
+
+#How to install
+
+- Go to https://bitly.com, sign up and generate your token: settings -> api -> Generate token
+
+- Download the project as .zip or clone the actual repository 
+
+- Create a file .env and put inside "bitly_token=YOUR_TOKEN"
+
+- Python3 should be already installed. Then use pip (or pip3, if there is a conflict with Python2) to install dependencies:
+
+```bash
+$python pip install -r requirements.txt
+```
+
+```bash
+$python python3 main.py "here is your long URL"
+```
+
+And that's it. You have got the bitly link. For example: bit.ly/3ieEuGB
+
+#Project Goals
+
+The code is written for educational purposes on online-course for web-developers dvmn.org.
---
+++ main.py
@@ -0,0 +1,67 @@
+import requests
+import os
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+import argparse
+
+
+def parse_url(link):
+    url = urlparse(link)
+    netloc = url.netloc
+    path = url.path
+    parsed_bitlink = f'{netloc}{path}'
+    return parsed_bitlink
+
+
+def is_bitlink(bitly_token, parsed_link):
+    headers = {'Authorization': bitly_token}
+    checking_url = f'https://api-ssl.bitly.com/v4/bitlinks/{parsed_link}'
+    response = requests.get(checking_url, headers=headers)
+    return response.ok
+
+
+def shorten_link(bitly_token, link):
+    headers = {'Authorization': bitly_token}
+    payload = {'long_url': link}
+    api_url = 'https://api-ssl.bitly.com/v4/bitlinks/'
+    response = requests.post(api_url, headers=headers, json=payload)
+    response.raise_for_status()
+    bitly_answer = response.json()
+    bitlink = bitly_answer['id']
+    return bitlink
+
+
+def count_clicks(bitly_token, bitlink):
+    headers = {'Authorization': bitly_token}
+    payload = {'unit': 'day', 'units': '-1'}
+    api_url = 'https://api-ssl.bitly.com/v4/bitlinks/'
+    clicks_url = f'{api_url}{bitlink}/clicks/summary'
+    response = requests.get(clicks_url, headers=headers, params=payload)
+    response.raise_for_status()
+    bitly_answer = response.json()
+    clicks_count = bitly_answer['total_clicks']
+    return clicks_count
+
+
+def main():
+    load_dotenv()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("link")
+    args = parser.parse_args()
+    link = args.link
+    bitly_token = os.environ['bitly_token']
+    try:
+        parsed_link = parse_url(link)
+        if is_bitlink(bitly_token, parsed_link):
+            total_clicks = count_clicks(bitly_token, parsed_link)
+            print('Это битлинк\n', 'Кол-во кликов', total_clicks)
+        else:
+            bitlink = shorten_link(bitly_token, link)
+            print('Битлинк', bitlink)
+    except requests.exceptions.HTTPError as e:
+        print('Введите корректный адрес ссылки')
+        raise SystemExit(e)
+
+
+if __name__ == '__main__':
+    main()
